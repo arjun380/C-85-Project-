6c4fd0ebfc1cfae5597bfb0f298dd5d543db4803
arjun380 <82092827+arjun380@users.noreply.github.com>
2021-07-15 08:35:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+greencar_width = 75;
+greencarheight = 100;
+
+
+background_image = "parkingLot.jpg";
+greencar = "car2.png";
+           
+greencar_x = 5;
+greencar_y = 225;
+
+
+function add()
+{
+	background_imaTag = new Image();
+	background_imaTag.onload = uploadBackground;
+	background_imaTag.src = greencar;
+
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadgreencar
+}
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+
+}
+
+function uploadgreencar() {
+	ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, rover_height);
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+    if(greencar_y >= 0 )
+    {
+        greencar_y -= 10;
+        console.log("when up arrow is pressed = " +  greencar_x + " - " +greencar_y);
+        uploadBackground();
+        uploadgreencar();
+    }
+}
+
+function down()
+{
+    if(greencar_y <=500 )
+    {
+        character_car_y += 10;
+        console.log("when up arrow is pressed = " +  greencar_x + " - " +greencar_y);
+        uploadBackground();
+        uploadgreencar();
+    }
+}
+
+function left()
+{
+    if(greencar_x >= 0 )
+    {
+        greencar_x -= 10;
+        console.log("when up arrow is pressed = " +  greencar_x + " - " +greencar_y);
+        uploadBackground();
+        uploadgreencar();
+    }
+}
+
+function right()
+{
+    if(greencar_x <=700 )
+    {
+        greencar_x += 10;
+        console.log("when up arrow is pressed = " +  greencar_x + " - " +greencar_y);
+        uploadBackground();
+        uploadgreencar();
+    }
+}
